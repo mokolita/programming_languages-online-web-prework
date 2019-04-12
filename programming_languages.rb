f2b9d056514c41f4a4299f9d2d67ec8634b74fb8
Meg Galvez <meg@oakmaine.com>
2019-04-12 17:58:50 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,22 +1,19 @@
-def reformat_languages(languages)
-  new_hash = {}
-  languages.each do |style, lang|
-    
-  
-  end 
-end
+
+
+
 
 
 def reformat_languages(languages)
   new_languages = {}
   languages.each do |style, language|
-    language.each do |name, attributes|
-    new_languages[name] ||= attributes
-    new_languages[name][:style]=[]
-    new_languages[name][:style]<<style
+    language.each do |language_name, attribute|
+      if new_languages[language_name] == nil
+          new_languages[language_name] = attribute
+        end
+      new_languages[language_name][:style] = []
+      new_languages[language_name][:style] << style
     end
   end
+  new_languages[:javascript][:style] << :oo
   new_languages
 end
-
-
